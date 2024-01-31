16b0182f0e5f22252e202212b506fd28b79cfe0f
Rohitmalvi123 <malvirohitabcd@gmail.com>
2024-01-31 16:50:52 -0500

    objects and constructor done

---
+++ 10_classes_and_oop/notes.md
@@ -0,0 +1,26 @@
+# javascript and classes
+
+## oop
+
+## object
+
+- collection of properties and methods
+- toLowerCase
+
+## why use oop
+
+## parts of oop
+
+Object literal
+
+- constructor function
+- Prototypes
+- Classes
+- instances (new, this)
+
+## 4 pillars
+
+Abstraction
+Encapsulation
+Inheritance
+Polymorphism
---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,38 @@
+// object literal
+
+const user = {
+  username: "Rohit",
+  loginCount: 8,
+  signdIn: true,
+
+  getUserDetails: function () {
+    // console.log("Got user details from database");
+    // console.log(`Username: ${this.username}`);
+    console.log(this);
+  },
+};
+
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this);
+
+// constructer function
+
+// const promiseOne = new promise();
+// const date = new Date();
+
+function User(username, logintCount, isLoggedIn) {
+  this.username = username;
+  this.loginCount = logintCount;
+  this.isLoggedIn = isLoggedIn;
+
+  this.greeting = function () {
+    console.log(`Welcome ${this.username}`);
+  };
+  return this;
+}
+
+const userOne = new User("Rohit", 12, true);
+const userTwo = new User("ChaiAurCode", 11, false);
+console.log(userOne.constructor);
+// console.log(userTwo);
