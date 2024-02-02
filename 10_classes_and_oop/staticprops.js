84c79f647c85bf8cdf3633c539ca28eb152ec7fc
Rohitmalvi123 <malvirohitabcd@gmail.com>
2024-02-02 12:30:15 -0500

    static method is done

---
+++ 10_classes_and_oop/staticprops.js
@@ -0,0 +1,27 @@
+class User {
+  constructor(username) {
+    this.username = username;
+  }
+
+  logeMe() {
+    console.log(`Username: ${this.username}`);
+  }
+
+  static creatId() {
+    return "123";
+  }
+}
+
+const rohit = new User("rohit");
+// console.log(rohit.creatId());
+
+class Teacher extends User {
+  constructor(username, email) {
+    super(username);
+    this.email = email;
+  }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com");
+iphone.logeMe();
+console.log(iphone.creatId());
